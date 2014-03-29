8300631cadb8dbb60123936a70c714e16c3e5953
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-29 18:33:47 +0200

    added tasks and tests for class Animal

---
+++ test_animal.py
@@ -0,0 +1,36 @@
+import animal
+import unittest
+
+
+class TestAnimal(unittest.TestCase):
+    def setUp(self):
+        self.a = animal.Animal("lion", 24, "sharik", "male", 150)
+
+    def test_animal_init(self):
+        self.assertEqual("lion", self.a.species)
+        self.assertEqual(24, self.a.age)
+        self.assertEqual("sharik", self.a.name)
+        self.assertEqual("male", self.a.gender)
+        self.assertEqual(150, self.a.weight)
+
+    def test_animal_grow(self):
+        self.a.grow(250, 7.5)
+        self.assertEqual(25, self.a.age)
+        self.assertEqual(157.5, self.a.weight)
+
+    def test_animal_grow_above_average(self):
+        self.a.grow(150, 5)
+        self.assertEqual(25, self.a.age)
+        self.assertEqual(150, self.a.weight)
+
+    def test_animal_feed(self):
+        result = self.a.feed(0.5)
+        self.assertEqual(75, result)
+
+    def test_animal_it_lives(self):
+        result = self.a._it_lives(24)
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
