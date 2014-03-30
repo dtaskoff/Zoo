fc96de67089b1185a0f49f3a310607321a794bc4
LachezarStoev <lachezarstoev93@gmail.com>
2014-03-30 04:19:26 +0300

    added get_last_breed method

--- database.py
+++ database.py
@@ -1,26 +1,44 @@
 import sqlite3
+
+
 class DB():
+
 	def __init__(self, db_name):
 		self.name = db_name
 		self.animal_conn = sqlite3.connect("animals.db")
 		self.zoo_conn = sqlite3.connect(db_name)
 		self.create_table()
+
 	def create_table(self):
 		c = self.zoo_conn
-		c.execute(''' CREATE TABLE IF NOT EXISTS {}
-			(id INTEGER PRIMARY KEY, name text, species text, age int, weight real, gender text)'''.format(self.name))
-		#c.execute(''' )
+		c.execute(''' CREATE TABLE IF NOT EXISTS zoo
+			(id INTEGER PRIMARY KEY, name text, species text, age int, weight real, gender text)''')
+		c.execute(''' CREATE TABLE IF NOT EXISTS breed
+			(animal_id int, last_breed int)''')
+
 	def get_food_type(self, species):
 		c = self.animal_conn.cursor()
-		food_type = c.execute("SELECT food_type FROM animals WHERE species=?",(species,)).fetchall()
-		return food_type[0][0]
+		food_type = c.execute("SELECT food_type FROM animals WHERE species=?",(species,)).fetchone()
+		return food_type[0]
+
 	def insert_animal(self, animal):
 		c = self.zoo_conn.cursor()
-		c.execute("INSERT INTO "+self.name+"(name, species, age, weight, gender) VALUES(?,?,?,?,?)",(animal.name,animal.species,animal.age,animal.weight,animal.gender))
-		#nito ? stava, nishto {}, nito %
-		#ako namerish reshenie kaji
+		c.execute("INSERT INTO zoo(name, species, age, weight, gender) VALUES(?,?,?,?,?)",(animal.name,animal.species,animal.age,animal.weight,animal.gender))
+		if animal.gender == "female":
+			id = c.execute("SELECT id FROM zoo").fetchall()
+			id = id[len(id)-1][0]
+			c.execute("INSERT INTO breed(animal_id, last_breed) VALUES(?,?)",(id,0)) #tyk nz kak se opredelq breed-a, moje bi 0 trqbva da e otnachalo
 		self.zoo_conn.commit()
+
 	def remove_animal(self, species, name):
 		c = self.zoo_conn.cursor()
-		c.execute("DELETE FROM "+self.name+" WHERE species=? and name=?",(species,name))
+		id_of_animal = c.execute("SELECT id FROM zoo WHERE species=? and name=?",(species, name)).fetchone()
+		if id_of_animal != []:
+			c.execute("DELETE FROM zoo WHERE species=? and name=?",(species,name))
+			c.execute("DELETE FROM breed WHERE animal_id=?",(str(id_of_animal[0]),))
 		self.zoo_conn.commit()
+	def get_last_breed(self, species, name):
+		c = self.zoo_conn.cursor()
+		id_of_animal = c.execute("SELECT id FROM zoo WHERE species=? and name=?",(species, name)).fetchone()
+		last_breed = c.execute("SELECT last_breed FROM breed WHERE animal_id=?",(str(id_of_animal[0]))).fetchone()
+		return last_breed[0]
--- test_database.py
+++ test_database.py
@@ -14,16 +14,32 @@ class DataBaseTest(unittest.TestCase):
 		self.db.insert_animal(animal)
 
 		c = self.db.zoo_conn.cursor()
-		animal_from_db = c.execute("SELECT  name, species, age, weight, gender FROM {}".format(self.db.name)).fetchall()
+		animal_from_db = c.execute("SELECT  name, species, age, weight, gender FROM zoo").fetchall()
 		self.assertEqual(("sharik","lion",24,150,"male"),animal_from_db[0])
+
+		animal_female = Animal("lion", 24, "Nia", "female", 150)
+		self.db.insert_animal(animal_female)
+		last_feed_from_db = c.execute("SELECT animal_id, last_breed FROM breed").fetchall()
+		self.assertEqual((2,0),last_feed_from_db[0])
+
 	def test_delete_animal(self):
-		animal = Animal("lion", 24, "sharik", "male", 150)
+		animal = Animal("lion", 24, "kirimitka", "female", 150)
 		self.db.insert_animal(animal)
 		self.db.remove_animal(animal.species,animal.name)
 
-		c=self.db.zoo_conn.cursor()
-		animal_from_db = c.execute("SELECT * FROM {}".format(self.db.name)).fetchall()
+		c = self.db.zoo_conn.cursor()
+		animal_from_db = c.execute("SELECT * FROM zoo").fetchall()
 		self.assertEqual([],animal_from_db)
+
+		breed_from_db = c.execute("SELECT * FROM breed").fetchall()
+		self.assertEqual([],breed_from_db)
+		
+	def test_get_last_breed(self):
+		animal = Animal("lion", 24, "Samanta", "female", 150)
+		self.db.insert_animal(animal)
+		last_breed = self.db.get_last_breed(animal.species, animal.name)
+		self.assertEqual(0, last_breed)
+
 	def tearDown(self):
 		call("rm " + self.db.name,shell=True)
 
