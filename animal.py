f5978fd3a24d25a01eec045c8c549be5e4f65909
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-04-01 21:14:03 +0300

    all tests working

--- animal.py
+++ animal.py
@@ -2,28 +2,28 @@ from random import randint
 
 
 class Animal():
-	def __init__(self, species, age, name, gender, weight):
-		self.species = species
-		self.age = age
-		self.name = name
-		self.gender = gender
-		self.weight = weight
-		
-	def __eq__(self, other):
-		return self.name == other.name and self.species == other.species
+    def __init__(self, species, age, name, gender, weight):
+        self.species = species
+        self.age = age
+        self.name = name
+        self.gender = gender
+        self.weight = weight
+        
+    def __eq__(self, other):
+        return self.name == other.name and self.species == other.species
 
-	def __str__(self):
-		return "{0}: {1}, {2}, {3}".format(self.name,
-					self.species, self.age, self.weight)
+    def __str__(self):
+        return "{0}: {1}, {2}, {3}".format(self.name,
+                    self.species, self.age, self.weight)
 
-	def grow(self, average_weight, weight_age_ratio):
-		if self.weight < average_weight:
-			self.weight += weight_age_ratio 
-		self.age += 1
+    def grow(self, average_weight, weight_age_ratio):
+        if self.weight < average_weight:
+            self.weight += weight_age_ratio 
+        self.age += 1
 
-	def lives(self, life_expectancy):
-		life_expectancy_in_months = life_expectancy * 12
-		return randint(1, life_expectancy_in_months) > self.age
-		
-	def feed(self, food_weight_ratio):
-		return food_weight_ratio * self.weight
+    def lives(self, life_expectancy):
+        life_expectancy_in_months = life_expectancy * 12
+        return randint(1, life_expectancy_in_months) > self.age
+        
+    def feed(self, food_weight_ratio):
+        return food_weight_ratio * self.weight
--- test_zoo.py
+++ test_zoo.py
@@ -6,45 +6,45 @@ from subprocess import call
 
 
 class TestZoo(unittest.TestCase):
-	def setUp(self):
-		self.zoo = Zoo(5, 50, Database("test_zoo.db"))
+    def setUp(self):
+        self.zoo = Zoo(5, 50, Database("test_zoo.db"))
 
-	def test_init(self):
-		self.assertEqual(5, self.zoo.capacity)
-		self.assertEqual(50, self.zoo.budget)
+    def test_init(self):
+        self.assertEqual(5, self.zoo.capacity)
+        self.assertEqual(50, self.zoo.budget)
 
-	def test_accommodate(self):
-		self.zoo.capacity = 0
-		self.assertEqual(False,self.zoo.accommodate("lion", 24, "sharik", "male", 150))
-		self.zoo.capacity = 5
-		animal = Animal("lion", 24, "sharik", "male", 150)
-		self.assertEqual(True,self.zoo.accommodate("lion", 24, "sharik", "male", 150))
-		self.assertEqual(animal,self.zoo.animals[0])
-		animals_from_db = self.zoo.database.zoo_conn.execute('''select * from zoo''').fetchall()
-		self.assertEqual(1, len(animals_from_db))
+    def test_accommodate(self):
+        self.zoo.capacity = 0
+        self.assertEqual(False,self.zoo.accommodate("lion", 24, "sharik", "male", 150))
+        self.zoo.capacity = 5
+        animal = Animal("lion", 24, "sharik", "male", 150)
+        self.assertEqual(True,self.zoo.accommodate("lion", 24, "sharik", "male", 150))
+        self.assertEqual(animal,self.zoo.animals[0])
+        animals_from_db = self.zoo.database.zoo_conn.execute('''select * from zoo''').fetchall()
+        self.assertEqual(1, len(animals_from_db))
 
-	def test_move_to_habitat(self):
-		self.zoo.accommodate("lion", 24, "sharik", "male", 150)
-		self.zoo.move_to_habitat("lion", "sharik")
-		self.assertEqual(0, len(self.zoo.animals))
-		animals_from_db = self.zoo.database.zoo_conn.execute('''select * from zoo''').fetchall()
-		self.assertEqual(0, len(animals_from_db))
+    def test_move_to_habitat(self):
+        self.zoo.accommodate("lion", 24, "sharik", "male", 150)
+        self.zoo.move_to_habitat("lion", "sharik")
+        self.assertEqual(0, len(self.zoo.animals))
+        animals_from_db = self.zoo.database.zoo_conn.execute('''select * from zoo''').fetchall()
+        self.assertEqual(0, len(animals_from_db))
 
-	def test_daily_incomes(self):
-		self.assertEqual(0,self.zoo.daily_incomes())
+    def test_daily_incomes(self):
+        self.assertEqual(0,self.zoo.daily_incomes())
 
-	def test_see_animals(self):
-		self.zoo.accommodate("lion", 24, "sharik", "male", 150)
-		expected = "sharik: lion, 24, 150"
-		self.assertEqual(expected, self.zoo.see_animals())
+    def test_see_animals(self):
+        self.zoo.accommodate("lion", 24, "sharik", "male", 150)
+        expected = "sharik: lion, 24, 150"
+        self.assertEqual(expected, self.zoo.see_animals())
 
-	def test_daily_expenses(self):
-		self.zoo.accommodate("lion", 24, "sharik", "male", 150)
-		expected = 150 * self.zoo.database.get_food_weight_ratio("lion")
-		self.assertEqual(expected * 4, self.zoo.daily_expenses())
+    def test_daily_expenses(self):
+        self.zoo.accommodate("lion", 24, "sharik", "male", 150)
+        expected = 150 * self.zoo.database.get_food_weight_ratio("lion")
+        self.assertEqual(expected * 4, self.zoo.daily_expenses())
 
-	def tearDown(self):
-		call("rm {}".format("test_zoo.db"),shell=True)
+    def tearDown(self):
+        call("rm {}".format("test_zoo.db"),shell=True)
 
 
 if __name__ == '__main__':
